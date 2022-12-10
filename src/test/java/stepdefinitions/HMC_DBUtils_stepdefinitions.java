d461e41b7b67f01c6ccf98d081fe89c6834e217d
Fatih Turker <fthturker80@gmail.com>
2022-12-10 14:06:51 +0300

    10.12.22

--- src/test/java/stepdefinitions/HMC_DBUtils_stepdefinitions.java
+++ src/test/java/stepdefinitions/HMC_DBUtils_stepdefinitions.java
@@ -13,11 +13,11 @@ public class HMC_DBUtils_stepdefinitions {
     @Given("kullanici DBUtils ile HMC veri tabanina baglanir")
     public void kullanici_db_utils_ile_hmc_veri_tabanina_baglanir() {
         DBUtils.getConnection();
-
     }
 
     @Given("kullanici DBUtils ile {string} tablosundaki {string} verilerini alir")
     public void kullanici_db_utils_ile_tablosundaki_verilerini_alir(String table, String field) {
+        //sorgu yapabilmemiz icin query lazim
         String query = "SELECT " + field + " FROM " + table;
         DBUtils.executeQuery(query);
 
@@ -25,10 +25,10 @@ public class HMC_DBUtils_stepdefinitions {
 
     @Given("kullanici DBUtils ile {string} sutunundaki verileri okur")
     public void kullanici_db_utils_ile_sutunundaki_verileri_okur(String field) throws SQLException {
+
         DBUtils.getResultset().first();
+        // DBUtils.getResultset().absolute(5); 5. yazdirmak icin
         System.out.println(DBUtils.getResultset().getString(field));
-
-
     }
 
 
@@ -52,7 +52,7 @@ public class HMC_DBUtils_stepdefinitions {
     @Then("DBUtills ile {int}. {string} in {int} oldugunu test eder")
     public void dbutillsIleInOldugunuTestEder(int istenenSiraNo, String field, int expectedDeger) throws SQLException {
 
-        DBUtils.getResultset().absolute(istenenSiraNo);
+        DBUtils.getResultset().absolute(istenenSiraNo); // 2. siraya gittik
         double actualDeger=DBUtils.getResultset().getDouble(field);
         System.out.println("expected deger : " + expectedDeger);
         System.out.println("actual deger : " + actualDeger);
--- src/test/resources/features/US1005_DBUtilsIleUpdate.feature
+++ src/test/resources/features/US1005_DBUtilsIleUpdate.feature
@@ -3,5 +3,5 @@ Feature: US1005 Kullanici database de update yapar
   Scenario: TC05 Kullanici update yapabilmeli
 
     Given kullanici DBUtils ile HMC veri tabanina baglanir
-    Then tHOTEL tablosunda IDHotel degeri 5 olan kaydin Email bilgisini "sizOldunuz@gmail.com" yapar
+    Then tHOTEL tablosunda IDHotel degeri 1016 olan kaydin Email bilgisini "sizOldunuz@gmail.com" yapar
     # UPDATE tHOTEL SET Email = 'sizOldunuz@gmail.com' WHERE IDHotel=1016;
